d32a32eab7f8151d60fba74b42289eb73719dc88
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-02 16:40:03 +0530

    Create 9b.py

---
+++ 9b.py
@@ -0,0 +1,23 @@
+count_python=0
+class Student():
+    count=0
+    pycount=0
+    def __init__(self,USN,Name,Subject):
+        self.USN=USN
+        self.Name=Name
+        self.Subject=Subject
+        if self.Subject.lower()=="python":
+            Student.pycount+=1
+        Student.count+=1
+listofstud=[]
+n=int(input("Enter the number of students "))
+for i in range(n):
+    name=input("Enter Name ")
+    usn=input("Enter USN ")
+    sub=input("Enter Subject ")
+    result=Student(usn,name,sub)
+    listofstud.append(d)
+for i in listofstud:
+    if i.Subject.lower()=="python":
+      print (i.Name,"Has taken Python")
+print (Student.pycount,"Have taken Python")
