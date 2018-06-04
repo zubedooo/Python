8ab1e5ea4792e109ad91aa9262c7b47d11ecc65b
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-04 21:48:11 +0530

    Update 8a.py

--- 8a.py
+++ 8a.py
@@ -5,12 +5,15 @@ def is_abecedarian(word):
             return False
         else:
             index +=1
-    return True
-    
-fin = open('words.txt')
+    return True   
+fin = open('words.txt','w+')
+fin.write("abcd qwsa ljkl wd xxx abcd\n dqdas\ndads\ndasd")
+fin.close()
+fin = open('words.txt','r+')
 count = 0
 for line in fin:
-    word = line.strip()
-    if is_abecedarian(word):
-        count += 1
+    for word in line.split():
+        if is_abecedarian(word):
+            count += 1
 print('There are {} abecedarian words.'.format(count))
+fin.close()
