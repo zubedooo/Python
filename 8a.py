51f2ca36e6d1953381e702a363b7c840b4e6794e
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-06-04 22:16:25 +0530

    Update 8a.py

--- 8a.py
+++ 8a.py
@@ -1,7 +1,7 @@
 def is_abecedarian(word):
     return(word==''.join(sorted(word)))
 file = open('words.txt','w+')
-file.write(input())
+file.write(input("Enter some words please: "))
 file.close()
 with open('words.txt','r+') as file:
     count=0
